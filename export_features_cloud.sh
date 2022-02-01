27888e001768d74aa204ca9fa152e7cf961093af
Cristiano Morais da Cunha <79987166+CMCunha@users.noreply.github.com>
2022-02-01 11:33:17 +0000

    Update export_features_cloud.sh

--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_URL=https://xray.cloud.xpand-it.com
+BASE_URL=https://xray.cloud.getxray.app
 KEYS="XT-41"
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
 curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v1/export/cucumber?keys=$KEYS" -o features.zip
